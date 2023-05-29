7c48081fe5dda696777bb8da6278582e24037794
Bernard Garekani <bernard.garekani@gmail.com>
2023-05-29 16:20:20 +0530

    Auth request change

--- YoutubeKit/API/Requests/SearchListRequest.swift
+++ YoutubeKit/API/Requests/SearchListRequest.swift
@@ -29,7 +29,7 @@ public struct SearchListRequest: Requestable {
         case .relatedToVideoID(_)?:
             return false
         case .none:
-            return false
+            return true
         }
     }
     
