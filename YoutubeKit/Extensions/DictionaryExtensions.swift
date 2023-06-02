77dc95827be323accf751e4533139ea7e3d78fb6
Bernard Garekani <bernard.garekani@gmail.com>
2023-06-02 11:47:03 +0530

    dateformate changed

--- YoutubeKit/Extensions/DictionaryExtensions.swift
+++ YoutubeKit/Extensions/DictionaryExtensions.swift
@@ -12,7 +12,7 @@ extension Dictionary where Key == String, Value == Any {
     mutating func appendingQueryParameter(key: String, value: Date?) {
         if let value = value {
             let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
             dateFormatter.timeZone = TimeZone.current
             self[key] = dateFormatter.string(from: value)
         }
